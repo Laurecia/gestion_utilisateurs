864d64d317588971947cb94422d3204fdb48e78a
Laurecia <KezaLaurences@gmail.com>
2020-09-17 16:07:27 +0400

    user.sh

---
+++ user.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+xls2csv list.xls > converted.csv
+
+input="/home/info/Documents/Embauche/converted.csv"
+array=($(awk -F : "{print $1} /etc/passwd ))
+i=0;
+
+while IFS=read -r line
+do
+         i=0
+         for item in ${array[*]}
+         do
+
+                 if ["$line" = "\"$item\""]; then
+                        i=$((i+1))
+
+                 fi
+           done
+           if ["si" = 0]; then
+                  echo "$line"
+                  useradd $line/
+            fi
+done < "$input"
